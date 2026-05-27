7b73959afbc52bb994b3e533f4021dc5f999bb72
iam-nadiii <naodd43@gmail.com>
2026-05-27 13:39:00 -0500

    added a time stamp to the receipt file handler so that the time an order was confirmed can be easily seen in all the receipt files

--- src/main/java/com/sando_nation/data/ReceiptFileHandler.java
+++ src/main/java/com/sando_nation/data/ReceiptFileHandler.java
@@ -10,12 +10,15 @@ public class ReceiptFileHandler {
 
     public boolean generateReceipt(Receipt receipt){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd\nHH:mm:ss");
         String fileName = receipt.getTime().format(formatter) + ".txt";
+        String timeStamp = receipt.getTime().format(formatter2);
         try {
             FileWriter fileWriter = new FileWriter("./Receipts/" + fileName);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
             bufferedWriter.write(String.valueOf(receipt.getOrder()));
+            bufferedWriter.write(timeStamp);
 
             bufferedWriter.close();
             return true;

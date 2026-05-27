fc103c025787ec03495511198feee34d79f73c1d
iam-nadiii <naodd43@gmail.com>
2026-05-26 20:13:29 -0500

    added the abstact class PremiumTopping.java which is extended by Meat.java and Cheese.java

--- src/main/java/com/sando_nation/model/Meat.java
+++ src/main/java/com/sando_nation/model/Meat.java
@@ -1,126 +1,13 @@
 package com.sando_nation.model;
 
-public class Meat extends Topping{
-        double priceFour, priceEight, priceTwelve, extraPriceFour, extraPriceEight, extraPriceTwelve;
-        public String size;
-        public boolean wantsExtra;
+public class Meat extends PremiumTopping {
 
-        public String getSize() {
-            return size;
-        }
-
-        public void setSize(String size) {
-            this.size = size;
-        }
-
-        public boolean isWantsExtra() {
-            return wantsExtra;
-        }
-
-        public void setWantsExtra(boolean wantsExtra) {
-            this.wantsExtra = wantsExtra;
-        }
-
-        public Meat (String name, double priceFour, double priceEight, double priceTwelve,
-                       double extraPriceFour, double extraPriceEight, double extraPriceTwelve) {
-            super(name);
-            this.priceFour        = priceFour;
-            this.priceEight       = priceEight;
-            this.priceTwelve      = priceTwelve;
-            this.extraPriceFour   = extraPriceFour;
-            this.extraPriceEight  = extraPriceEight;
-            this.extraPriceTwelve = extraPriceTwelve;
-        }
-
-        public double getPriceFour() {
-            return priceFour;
-        }
-
-        public void setPriceFour(double priceFour) {
-            this.priceFour = priceFour;
-        }
-
-        public double getPriceEight() {
-            return priceEight;
-        }
-
-        public void setPriceEight(double priceEight) {
-            this.priceEight = priceEight;
-        }
-
-        public double getPriceTwelve() {
-            return priceTwelve;
-        }
-
-        public void setPriceTwelve(double priceTwelve) {
-            this.priceTwelve = priceTwelve;
-        }
-
-        public double getExtraPriceFour() {
-            return extraPriceFour;
-        }
-
-        public void setExtraPriceFour(double extraPriceFour) {
-            this.extraPriceFour = extraPriceFour;
-        }
-
-        public double getExtraPriceEight() {
-            return extraPriceEight;
-        }
-
-        public void setExtraPriceEight(double extraPriceEight) {
-            this.extraPriceEight = extraPriceEight;
-        }
-
-        public double getExtraPriceTwelve() {
-            return extraPriceTwelve;
-        }
-
-        public void setExtraPriceTwelve(double extraPriceTwelve) {
-            this.extraPriceTwelve = extraPriceTwelve;
-        }
-
-        public double calculatePrice(String size, boolean wantsExtra){
-            if(size.equalsIgnoreCase("4 inch")){
-                if (wantsExtra){
-                    return priceFour;
-                }
-                return extraPriceFour;
-            }
-
-            if(size.equalsIgnoreCase("8 inch")){
-                if (wantsExtra){
-                    return priceEight;
-                }
-                return extraPriceEight;
-            }
-
-            if (size.equalsIgnoreCase("12 inch")){
-                if (wantsExtra){
-                    return priceTwelve;
-                }
-                return extraPriceTwelve;
-            }
-            return 0;
-        }
-
-         @Override
-        public double getPrice(){
-            return calculatePrice(this.size, this.wantsExtra);
-        }
-
-        @Override
-        public String toString() {
-            return "Cheese{" +
-                    "priceFour=" + priceFour +
-                    ", priceEight=" + priceEight +
-                    ", priceTwelve=" + priceTwelve +
-                    ", extraPriceFour=" + extraPriceFour +
-                    ", extraPriceEight=" + extraPriceEight +
-                    ", extraPriceTwelve=" + extraPriceTwelve +
-                    ", name='" + name + '\'' +
-                    '}';
-        }
+    public Meat(String name, double priceFour, double priceEight, double priceTwelve,
+                double extraPriceFour, double extraPriceEight, double extraPriceTwelve) {
+        super(name, priceFour, priceEight, priceTwelve,
+                extraPriceFour, extraPriceEight, extraPriceTwelve);
     }
 
-
+    @Override
+    public String getDescription() { return "Meat: " + getName(); }
+}
---
+++ src/main/java/com/sando_nation/model/PremiumTopping.java
@@ -0,0 +1,37 @@
+package com.sando_nation.model;
+
+public abstract class PremiumTopping extends Topping {
+    protected double priceFour, priceEight, priceTwelve;
+    protected double extraPriceFour, extraPriceEight, extraPriceTwelve;
+    protected boolean wantsExtra;
+
+    public PremiumTopping(String name, double priceFour, double priceEight, double priceTwelve,
+                          double extraPriceFour, double extraPriceEight, double extraPriceTwelve) {
+        super(name);
+        this.priceFour        = priceFour;
+        this.priceEight       = priceEight;
+        this.priceTwelve      = priceTwelve;
+        this.extraPriceFour   = extraPriceFour;
+        this.extraPriceEight  = extraPriceEight;
+        this.extraPriceTwelve = extraPriceTwelve;
+    }
+
+    public boolean isWantsExtra()           { return wantsExtra; }
+    public void    setWantsExtra(boolean w) { this.wantsExtra = w; }
+
+    public double calculatePrice(String size, boolean wantsExtra) {
+        if (size == null) return 0.00;
+        if (size.equalsIgnoreCase("4 inch"))
+            return wantsExtra ? extraPriceFour  : priceFour;
+        if (size.equalsIgnoreCase("8 inch"))
+            return wantsExtra ? extraPriceEight : priceEight;
+        if (size.equalsIgnoreCase("12 inch"))
+            return wantsExtra ? extraPriceTwelve : priceTwelve;
+        return 0.00;
+    }
+
+    @Override
+    public double getPrice() {
+        return calculatePrice(this.getSize(), this.wantsExtra);
+    }
+}
